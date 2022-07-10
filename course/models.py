51e11e909b33044eeccd10fb73dc6e13715cd253
therollingambit <therollingambit@gmail.com>
2022-07-10 21:17:35 +0800

    feat: add comment model

--- course/models.py
+++ course/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Category(models.Model):
@@ -47,3 +48,11 @@ class Lesson(models.Model):
 
   def __str__(self):
     return self.title
+
+class Comment(models.Model):
+  course = models.ForeignKey(Course, related_name='comments', on_delete=models.CASCADE)
+  lesson = models.ForeignKey(Lesson, related_name='comments', on_delete=models.CASCADE)
+  name = models.CharField(max_length=100)
+  content = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  created_by = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
--- course/views.py
+++ course/views.py
@@ -3,23 +3,46 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from .serializers import CourseListSerializer, CourseDetailSerializer, LessonListSerializer
-from .models import Course, Lesson
+from .serializers import CourseListSerializer, CourseDetailSerializer, LessonListSerializer, CommentsSerializer
+from .models import Course, Lesson, Comment
+
 
 @api_view(['GET'])
 def get_courses(request):
-  courses = Course.objects.all()
-  serializer = CourseListSerializer(courses, many=True)
-  return Response(serializer.data)
+    courses = Course.objects.all()
+    serializer = CourseListSerializer(courses, many=True)
+    return Response(serializer.data)
+
 
 @api_view(['GET'])
 def get_course(request, slug):
-  course = Course.objects.get(slug=slug)
-  course_serializer = CourseDetailSerializer(course)
-  lesson_serializer = LessonListSerializer(course.lessons.all(), many=True)
+    course = Course.objects.get(slug=slug)
+    course_serializer = CourseDetailSerializer(course)
+    lesson_serializer = LessonListSerializer(course.lessons.all(), many=True)
 
-  data = {
-    'course': course_serializer.data,
-    'lessons': lesson_serializer.data,
-  }
-  return Response(data)
+    data = {
+        'course': course_serializer.data,
+        'lessons': lesson_serializer.data,
+    }
+    return Response(data)
+
+
+@api_view(['GET'])
+def get_comments(request, course_slug, lesson_slug):
+  lesson = Lesson.objects.get(slug=lesson_slug)
+  serializer = CommentsSerializer(lesson.comments.all(), many=True)
+  return Response(serializer.data)
+
+@api_view(['POST'])
+def add_comment(request, course_slug, lesson_slug):
+    data = request.data
+    name = data.get('name')
+    content = data.get('content')
+
+    course = Course.objects.get(slug=course_slug)
+    lesson = Lesson.objects.get(slug=lesson_slug)
+
+    comment = Comment.objects.create(
+        course=course, lesson=lesson, name=name, content=content, created_by=request.user)
+
+    return Response({'message': 'The comment was added!'})
